d2916f69c929d7dd531e17ea76547a18c5c36cf9
DipanjanKhan <125537480+DipanjanKhan@users.noreply.github.com>
2024-03-18 14:41:17 +0530

    Initial commit

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import os
 import videoSearch
 
 load_dotenv('.env')
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
